4dab2f193e29bd6d57d46537e6b54e29c9df649a
袁其亮 <yuanqiliang@51kik.com>
2016-10-22 14:49:39 +0800

    添加devtools

---
+++ demo-microservice-web/src/main/java/com/lance/demo/microservice/App.java
@@ -0,0 +1,25 @@
+package com.lance.demo.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import java.util.Arrays;
+
+/**
+ * Created by perdonare on 2016/10/20.
+ */
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext ctx = SpringApplication.run(App.class, args);
+
+        System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+        String[] beanNames = ctx.getBeanDefinitionNames();
+        Arrays.sort(beanNames);
+        for (String beanName : beanNames) {
+            System.out.println(beanName);
+        }
+    }
+}
---
+++ demo-microservice-web/src/main/java/com/lance/demo/microservice/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.lance.demo.microservice.controller;
+
+/**
+ * Created by perdonare on 2016/10/20.
+ */
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+}
