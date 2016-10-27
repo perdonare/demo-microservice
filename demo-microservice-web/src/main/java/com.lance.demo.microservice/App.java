c531f58dcdb5a6a01bfd2bfe6ed218dc5f33dfbb
袁其亮 <yuanqiliang@51kik.com>
2016-10-27 15:07:06 +0800

    spring cloud init

---
+++ demo-microservice-web/src/main/java/com.lance.demo.microservice/App.java
@@ -0,0 +1,14 @@
+package com.lance.demo.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by perdonare on 2016/10/27.
+ */
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class,args);
+    }
+}
